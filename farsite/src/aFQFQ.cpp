993b1f1828326e96c1108d83a4ced89822e74245
Fangqierin <fangqil2@uci.edu>
2022-02-23 17:36:40 -0800

    farsite

---
+++ farsite/src/aFQFQ.cpp
@@ -0,0 +1,14 @@
+//#include <stdexcept>
+//#include <cstdlib>
+#include <iostream>
+//#include "arc.hpp"
+//#include "boost/filesystem/fstream.hpp"
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main() {
+   double d = 238649.21316934;
+   string s = to_string(d);
+   cout << "Conversion of double to string: " << s;
+   return 0;
+}
