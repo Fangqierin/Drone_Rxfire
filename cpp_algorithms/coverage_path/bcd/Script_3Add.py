9c410457fec564d20acc5f8696bccd6e17acdc5a
Fangqierin <fangqil2@uci.edu>
2022-10-19 13:49:28 -0700

    ,,

--- cpp_algorithms/coverage_path/bcd/Script_3Add.py
+++ cpp_algorithms/coverage_path/bcd/Script_3Add.py
@@ -9,4 +9,3 @@ nohup python3 FQ_Com_Packed.py 25 60 3 >& /tmp/log_25_60_3.txt 2>&1  &
 nohup python3 FQ_Com_Packed.py 5 1 3 >& /tmp/log_5_1_3.txt 2>&1  &
 nohup python3 FQ_Com_Packed.py 10 1 3 >& /tmp/log_10_1_3.txt 2>&1  &
 nohup python3 FQ_Com_Packed.py 15 1 3 >& /tmp/log_15_1_3.txt 2>&1  &
-l
